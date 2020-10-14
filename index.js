346aeb4f7d8b144a9d5264ca75acd0b600e1981e
Adil Rehman Dar <adil.dar@naxxa.io>
2020-10-14 06:42:53 -0700

    adding new function

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const AWS = require('aws-sdk');
-const { v4: uuid } = require('uuid');
 const express = require('express');
 const app = express()
 const http = require('http')
 const PORT = process.env.port || 3000
 var morgan = require('morgan')
-const chime = new AWS.Chime({ region: 'us-east-1' });
+var AWS = require('aws-sdk');
+AWS.config.update({region:'us-east-1'})
+var docClient = new AWS.DynamoDB.DocumentClient();
 var welcomeNotes = require("./welcomeNote.json")
-chime.endpoint = new AWS.Endpoint('https://service.chime.aws.amazon.com/console')
 const server = http.createServer(app)
 server.listen(PORT, () => {
   console.log("server started in port ", PORT)
@@ -18,6 +17,42 @@ app.get('/welcomeNote', (req, res, next) => {
   console.log(welcomeNotes[radNumber]);
   const data = welcomeNotes[radNumber]
   res.status(200).json(data);
+})
+app.get('/getUserInfo', async (req, res, next) => {
+  const response = {
+    succee: false,
+    error: false,
+    data: null,
+    message: null
+  }
+  console.log()
+  if (req.query.userId) {
+    await getUserByUserId(req.query.userId).then(user => {
+      console.log(user, "res")
+      if (user) {
+        // response.body.userId = res._id;
+        response.succee = true;
+        response.error = false;
+        response.data = user;
+        response.message = "User Profile Found"
+        res.status(200).json(response);
+      } else {
+        response.succee = false
+        response.error = true;
+        response.data = null;
+        response.message = "User not found"
+        res.status(404).json(response);
+      }
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+  } else {
+    response.succee = false
+    response.error = true;
+    response.data = null;
+    res.status(400).json(err);
+  }
 })
 app.use('/', async (req, res, next) => {
   const meetingResponse = await chime.createMeeting({
@@ -39,3 +74,50 @@ app.use('/', async (req, res, next) => {
 
 
 
+async function getUserByUserId(userId) {
+  return await new Promise(async (resolve, reject) => {
+    var params = {
+      TableName: "dev-users",
+    // FilterExpression: "#UserName = :username",
+    // ExpressionAttributeNames:{
+    //     "#UserName": "UserName"
+    // },
+    // ExpressionAttributeValues: {
+    //     ":username": username
+    // }
+      KeyConditionExpression: "#id = :userID",
+       ExpressionAttributeNames:{
+          "#id": "_id"
+      },
+      ExpressionAttributeValues: {
+        ":userID": userId
+      }
+    };
+
+    docClient.query(params, function (err, data) {
+      if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        reject(err)
+      } else {
+        console.log("Query succeeded.", data);
+        resolve(data.Items[0])
+      }
+    });
+  //   MongoClient.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
+  //     if (err) {
+  //       console.log(err, "error")
+  //       reject(err)
+  //     } else {
+  //       const dbo = db.db(dataBaseName)
+  //       dbo.collection("users").find({ _id: ObjectId(userId) }).toArray(function (err, result) {
+  //         if (err) {
+  //           reject(err)
+  //         };
+  //         console.log(result);
+  //         resolve(result[0])
+  //         db.close();
+  //       });
+  //     }
+  //   });
+  })
+}
